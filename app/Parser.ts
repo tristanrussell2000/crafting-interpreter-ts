1621ebbdd781939aaca17cc00742d9d285e7bbf7
Tristan Russell <tristanrussell2000@gmail.com>
2025-03-10 13:26:26 -0700

    For loops

--- app/Parser.ts
+++ app/Parser.ts
@@ -119,11 +119,52 @@ export class Parser {
     }
 
     #statement(): Stmt {
+        if (this.#match(TokenType.FOR)) return this.#forStatement();
         if (this.#match(TokenType.IF)) return this.#ifStatement();
         if (this.#match(TokenType.PRINT)) return this.#printStatement();
         if (this.#match(TokenType.LEFT_BRACE)) return new Block(this.#block());
         if (this.#match(TokenType.WHILE)) return this.#whileStatement();
         return this.#expressionStatement();
+    }
+
+    #forStatement(): Stmt {
+        this.#consume(TokenType.LEFT_PAREN, "Expect '(' after 'for'.");
+
+        let initializer: Stmt | null;
+        if (this.#match(TokenType.SEMICOLON)) {
+            initializer = null;
+        } else if (this.#match(TokenType.VAR)) {
+            initializer = this.#varDeclaration();
+        } else {
+            initializer = this.#expressionStatement();
+        }
+
+        let condition: Expr | null = null;
+        if (!this.#check(TokenType.SEMICOLON)) {
+            condition = this.#expression();
+        }
+        this.#consume(TokenType.SEMICOLON, "Expect ';' after loop condition.");
+
+        let increment: Expr | null = null;
+        if (!this.#check(TokenType.RIGHT_PAREN)) {
+            increment = this.#expression();
+        }
+        this.#consume(TokenType.RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        let body = this.#statement();
+
+        if (increment !== null) {
+            body = new Block([body, new Expression(increment)]);
+        }
+
+        if (condition === null) condition = new Literal(true);
+        body = new While(condition, body);
+
+        if (initializer !== null) {
+            body = new Block([initializer, body]);
+        }
+
+        return body;
     }
 
     #whileStatement(): Stmt {
